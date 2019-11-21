29e65b66dd6a75f9530e0b7d266a1b49333d9ffd
MercedesDavila <mmdavila@outlook.com>
2019-11-21 12:35:09 -0600

    Se agrega Json

---
+++ app.js
@@ -0,0 +1,7 @@
+fetch('./gaso.json')
+.then(response => response.json())
+.then(data => {
+  console.log(data) // Prints result from `response.json()` in getRequest
+})
+.catch(error => console.error(error))
+
