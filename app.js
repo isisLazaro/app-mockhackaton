d24b722b39152bc32369898af6a83a431bf13ac9
Isis Lázaro <51394610+isisLazaro@users.noreply.github.com>
2019-11-21 21:16:51 -0600

    quitando código repetido

--- app.js
+++ app.js
@@ -54,38 +54,3 @@ function noGet() {
   alert("Porfavor habilita el permiso para compartir ubicación");
 }
 
-function getMap(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let latLng = new google.maps.LatLng(latitude, longitude);
-  let objConfig = {
-    zoom: 19,
-    center: latLng
-  };
-  let map = new google.maps.Map(root, objConfig);
-  marker = new google.maps.Marker({
-    position: latLng,
-    map: map,
-    title: "Estás Aquí"
-    // animation: google.maps.Animation.BOUNCE
-  });
-}
-const urlApi =
-  //   "https://crossorigin.me/https://api-gas-stations-mex.herokuapp.com/gasstations";
-  "https://cors-anywhere.herokuapp.com/https://api-gas-stations-mex.herokuapp.com/gasstations";
-
-const fetchApi = async urlApi => {
-  try {
-    const response = await fetch(urlApi);
-    const data = await response.json();
-    //console.log(data[1].location);
-    //return data;
-    data.map(element => {
-      console.log(element.location);
-    });
-  } catch (error) {
-    return error;
-  }
-};
-
-fetchApi(urlApi);
